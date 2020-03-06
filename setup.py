e445ebec5a7239fb1e40eb998290e008c26dccc0
SamSchott <ss2151@cam.ac.uk>
2020-03-06 21:36:46 +0000

    updated python classfiers

--- setup.py
+++ setup.py
@@ -46,5 +46,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
